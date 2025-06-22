8b89e9d6d84d8d8e05bdc383a30ebd7ce4a83bdc
MazaFaaka <haitovvasyl@gmail.com>
2025-06-22 20:54:10 +0200

    compled standart sentg

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,11 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = (async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, { encoding: 'utf-8' });
+    console.log('readingFile:', data);
+  } catch (error) {
+    console.error('error readingFile:', error.message);
+  }
+})();
